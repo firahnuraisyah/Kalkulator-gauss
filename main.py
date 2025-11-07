7b9e7f628885731927451d62380bd0f9232c5c6a
zahwayy <zahwaputry279@gmail.com>
2025-11-07 10:03:19 +0800

    tes2

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-#komen ges
 import sys
 from gauss_solver import input_matrix, normalize, eliminate, back_substitution, analyze_result, save_to_file
-
+#halo
 class App:
     def __init__(self):
         self.solver = GaussSolver()
