3cea2d150559b10d671f9d8557254a87016df31b
firahnuraisyah <nuraisyahfirah@gmail.com>
2025-11-10 10:25:15 +0800

    hapus login

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import sys
 from gauss_solver import input_matrix, normalize, eliminate, back_substitution, analyze_result, save_to_file
-#halo
+
 class App:
     def __init__(self):
         self.solver = GaussSolver()
-        self.user = UserLogin()
 
     def run(self):
-        if self.user.login():
-            self.menu()
+        self.menu()
